ba86228909bc12c09ce79ef12dbbbefd557e2bf0
zain <annexergk@gmail.com>
2019-07-14 12:44:41 +0500

    """Initial commit"""

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#with open('textFile.txt','r') as file:
+#    #for line in file:
+ #   print(line)
+#print(file.readlines()) # will input the text in a list with \n
+
+#with open('textfile.txt','r') as file: # to read a file
+#   print(file.read())    
+#  element = file.readlines()[1] # print(element.rstrip())
+# toppa hai tu
+def add(a,b): #calculator
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b 
+def div(a,b):
+    return a/b   
